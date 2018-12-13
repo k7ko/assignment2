255ac2306f695fb9832e70b21e9ede298ccad317
k7ko <pkikomeko1@gmail.com>
2018-12-13 16:26:32 +0300

    add pyhton assignment

---
+++ andelaLevelUp.py
@@ -0,0 +1,16 @@
+name = "Whats My Name...?"
+#for letter in name:
+   # print(letter)
+
+ages = [23, 45, 56, 76, 56]
+ages.insert(2, 77)
+print(ages)
+ages.remove(23)
+print(ages)
+
+capitals = {
+    "Uganda" : "Kampala",
+    "Kenya" : "Nairobi",
+    "Tanzania" : "Dodoma"
+}
+print(capitals["Uganda"])
